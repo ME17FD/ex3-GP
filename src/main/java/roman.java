38582c77666b332ca73e79408e0f126b5396954b
mehdi <faddak.mehdi@gmail.com>
2024-12-13 11:27:00 +0100

    updated code, test passed for big nums and 0 and negatives

--- src/main/java/roman.java
+++ src/main/java/roman.java
@@ -4,8 +4,8 @@ public class roman {
         public static String convertirEnRomains(int nombre) {
             
     
-            int[] valeurs = { 100, 90, 50, 40, 10, 9, 5, 4, 1};
-            String[] symboles = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+            int[] valeurs = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] symboles = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     
             StringBuilder resultat = new StringBuilder();
     
